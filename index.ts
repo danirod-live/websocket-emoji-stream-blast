9bb2114d15e5c0c0b7d46a507404ec862d5a44c8
Dani Rodríguez <dani@danirod.es>
2022-04-27 22:32:01 +0200

    Create experiment

---
+++ index.ts
@@ -0,0 +1,34 @@
+import fastify from "fastify";
+import fastifyIO from "fastify-socket.io";
+import fastifyStatic from "@fastify/static";
+import { join } from 'path';
+
+const server = fastify();
+server.register(fastifyIO);
+server.register(fastifyStatic, {
+  root: join(__dirname, 'public'),
+  prefix: '/',
+});
+
+server.ready().then(() => {
+  server.io.on("connection", (socket) => {
+    console.log("HOLA");
+    socket.on("hello", () => {
+      console.log("Nos dice hola");
+    });
+    socket.on("client", () => {
+      console.log("se identifica como cliente");
+    });
+    socket.on("server", () => {
+      console.log("se identifica como servidor");
+      socket.join("obs");
+    });
+    socket.on("emoji", (emoji) => {
+      console.log("emoji: ", emoji);
+      socket.to("obs").emit("emoji", emoji);
+    });
+  });
+});
+
+console.log("Estamos a la escucha");
+server.listen(3000);
